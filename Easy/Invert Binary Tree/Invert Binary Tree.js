38a44288a333f6b3f62c4b00b12405665c39be3d
Rob <zero-hour2010@hotmail.com>
2025-03-18 16:15:19 -0500

    Invert Binary Tree algorithm

---
+++ Easy/Invert Binary Tree/Invert Binary Tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function invertTree(root) {
+    if (root == null) {
+        return null;
+    }
+    var temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
+}
+;
