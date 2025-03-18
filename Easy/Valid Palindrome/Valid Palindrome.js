a41cc337bffb5ef7ffbc6e6dea7ed37072049a21
Rob <zero-hour2010@hotmail.com>
2025-03-17 22:15:31 -0500

    Improved algorithm for valid palindrome

--- Easy/Valid Palindrome/Valid Palindrome.js
+++ Easy/Valid Palindrome/Valid Palindrome.js
@@ -1,17 +1,9 @@
 function isPalindrome(s) {
-    s = s.toLowerCase();
-    var origString = s;
-    for (var _i = 0, s_1 = s; _i < s_1.length; _i++) {
-        var char = s_1[_i];
-        if (!/^[a-z0-9]+$/i.test(char)) {
-            s = s.replace(char, "");
-        }
-    }
+    var formattedString = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
     var startPointer = 0;
-    var endPointer = s.length - 1;
-    for (var i = 0; i < s.length; i++) {
-        // console.log("left pointer is: " + s[startPointer]+ " right pointer is: " + s[endPointer])
-        if (s[startPointer] != s[endPointer]) {
+    var endPointer = formattedString.length - 1;
+    while (startPointer < endPointer) {
+        if (formattedString.charAt(startPointer) !== formattedString.charAt(endPointer)) {
             return false;
         }
         startPointer++;
--- Easy/Valid Palindrome/Valid Palindrome.ts
+++ Easy/Valid Palindrome/Valid Palindrome.ts
@@ -1,15 +1,9 @@
 function isPalindrome(s: string): boolean {
-    s=s.toLowerCase();
-    for(const char of s){
-        if(!/^[a-z0-9]+$/i.test(char)){
-            s=s.replace(char,"");
-        }
-    }
-    let startPointer: number = 0;
-    let endPointer: number = s.length-1;
-    for(let i: number = 0; i < s.length; i++){
-        // console.log("left pointer is: " + s[startPointer]+ " right pointer is: " + s[endPointer])
-        if(s[startPointer] != s[endPointer]){
+    const formattedString: string = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let startPointer = 0;
+    let endPointer = formattedString.length-1;
+    while(startPointer < endPointer){
+        if(formattedString.charAt(startPointer) !== formattedString.charAt(endPointer)){
             return false;
         }
         startPointer++;
@@ -17,6 +11,7 @@ function isPalindrome(s: string): boolean {
     }
     return true;
 };
+
 
 let testString1: string = "A man, a plan, a canal: Panama";
 const testString2: string = "race a car";
