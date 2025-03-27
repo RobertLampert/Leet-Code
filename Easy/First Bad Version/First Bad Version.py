8c8961aefbcbedbde18f2398e527b31cd51f818b
Rob <zero-hour2010@hotmail.com>
2025-03-27 17:25:55 -0500

    First bad version algorithm

--- Easy/First Bad Version/First Bad Version.py
+++ Easy/First Bad Version/First Bad Version.py
@@ -5,10 +5,18 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        for i in range(n):
-            if isBadVersion(i):
-                return i
-
+        left = 0
+        right = n
+        foundBadVersion = -1
+        while left<=right:
+            mid = int((left+right)/2)
+            if isBadVersion(mid):
+                found = mid
+                right = mid-1
+            else:
+                left = mid + 1
+        return found
+        
 
 
 
