4a300102bf779358b37a226d7ed225dbfc0a9f53
Rob <zero-hour2010@hotmail.com>
2025-04-06 17:36:04 -0500

    Counting bits algorithm dynamic programming approach - easy

--- Easy/Counting Bits/Counting Bits.py
+++ Easy/Counting Bits/Counting Bits.py
@@ -4,12 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        numOneArr = []
-        for i in range(n+1):
-            binNum = bin(i)
-            numOnes = binNum.count("1")
-            numOneArr.append(numOnes)
+        if n == 0:
+            numOneArr = [0]
+            return numOneArr
+        
+        numOneArr = [0,1]
+        for i in range(2,n+1):
+            if i % 2 == 0:
+                numOneArr.append(numOneArr[int(i/2)])
+            else:
+                numOneArr.append(numOneArr[i-1]+1)
         return numOneArr
-
-solution = Solution()
-print(solution.countBits(5))
